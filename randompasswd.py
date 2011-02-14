6bffd2eb17507ad0e6355c64b959d43f447df3bf
Sam Parkinson <r3morse@gmail.com>
2011-02-14 00:39:30 +0000

    improved commenting.

--- randompasswd.py
+++ randompasswd.py
@@ -1,20 +1,38 @@
 # -*- coding: UTF8 -*-
 #
-# This class (RandomPasswd) provides a method of generating a password using
-# random data from random.org (thanks to Clovis Fabricio: http://pypi.python.org/pypi/randomdotorg/)
+# RandomPasswd
+# ============
 #
-# To use, use the function passwd() in the form:
+# This class provides a method of generating a secure password.
+# Dependant on random.org and the python class found here:
+# http://pypi.python.org/pypi/randomdotorg/
 #
-# p = RandomPasswd()
-# password = p.passwd(length, security_level[1-4])
+# Example useage:
 #
-# Security level 1 is low, 4 is high.
+# >>> from randompasswd import RandomPasswd
+# >>> p = RandomPasswd()
+# >>> print p.passwd([length], [strength], [ammount])
+#
+# A password strength of 1 is low while 4 is very high.
+
+__version__ = '1.0'
+__url__ = 'https://github.com/r3morse/random-passwd'
+__author__ = "Sam Parkinson <r3morse at gmail dot com>"
+__license__ = "MIT Licence"
 
 from randomdotorg import RandomDotOrg
 
 r = RandomDotOrg()
 
 class RandomPasswd():
+    """This class can be used to generate secure passwords.
+
+    To use do something like the following:
+    
+    >>> from randompasswd import RandomPasswd
+    >>> p = RandomPasswd()
+    >>> print p.passwd()
+    """
 
     # A list of characters that will be used to generate a password.
     lower   = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
@@ -29,31 +47,31 @@ class RandomPasswd():
                 "-", "_", "=", "+", "[", "{", "]", "}", ";", ":", "?",
                 "@", "#", "~", "\\", "|", ",", "<", ".", ">", "/" ]
 
-    quota   = None
-
     # Creating the chars variable that will, according to the security level
     # contain a list of characters from the above lists.
-    chars = dict()
+    chars   = dict()
 
-    # Takes the given security level and generates a mixed list for the chars var
-    def set_security(self, level):
-        level = int(round(level))
+    # Our quota at random.org
+    quota   = None
+
+    def set_security(self, strength):
+        """Takes the given security strength and generates a mixed list in `chars`."""
+        level = int(round(strength))
         
-        if (level >= 1 and level <= 4):
-            if level >= 1:
+        if (strength >= 1 and strength <= 4):
+            if strength >= 1:
                 self.chars = self.lower
-            if level >= 2:
+            if strength >= 2:
                 self.chars = self.chars + self.upper
-            if level >= 3:
+            if strength >= 3:
                 self.chars = self.chars + self.number
-            if level == 4:
+            if strength == 4:
                 self.chars = self.chars + self.symbol
         else:
-            raise TypeError("Please enter a security level between 1 and 4.")
-            
-    # Function retrives the random string of numbers required to generate a password
-    # from random.org
+            raise ValueError("Please enter a security level between 1 and 4.")
+    
     def get_randnumbers(self, length):
+        """Retrives the random string of numbers for our password."""
         if type(length) is type(int()):
             data = list()
 
@@ -70,8 +88,8 @@ class RandomPasswd():
         else:
              raise TypeError("The length must be an intger.")
 
-    # Converts the list of numbers into a list of characters using the chars var
     def list_2_char(self, numbers):
+        """Converts the list of numbers into a list of characters using `chars`."""
         data = list()
 
         for i in numbers:
@@ -79,8 +97,8 @@ class RandomPasswd():
 
         return data
 
-    # Generates a string of the actual password from the list of characters
     def gen_password(self, letters):
+        """Generates string(s) of the actual password from the list `letters`."""
         password = str()
 
         for i in letters:
@@ -88,13 +106,24 @@ class RandomPasswd():
 
         return password
 
-    # The public method for generating a password, input is length of password and security level
-    # default is a length of 4, security level 4 and ammount of 1.
-    def passwd(self, length = 12, level = 4, ammount = 1):
+    # The public method for generating a password
+    # default is a length of 4, security strength 4 and ammount of 1.
+    def passwd(self, length = 12, strength = 4, ammount = 1):
+        """Generates a strong password with a default length of 12.
+        
+        `length`   the length of the password(s).
+        
+        `strength` the strength of the password(s), 1 is low
+                   4 is very hiht.
+        
+        `ammount`  the number of passwords you would like to make,
+                   when more than one is made, this function
+                   returns a list of strings.
+        """
         if type(ammount) is not type(int()) or ammount < 1:
             raise TypeError("Ammount needs to be an intger.")
 
-        self.set_security(level)
+        self.set_security(strength)
 
         password = list()
         i = 0
