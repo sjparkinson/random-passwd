95084e0eab229640452f1ae2c150681ba6862589
Sam Parkinson <sparkinson@outlook.com>
2013-01-28 20:25:12 +0000

    Code review.
    Bit of refractoring, removed some pointless variables and tidied up the
    code.

--- randompasswd.py
+++ randompasswd.py
@@ -39,21 +39,21 @@ class RandomPasswd:
     """
 
     # A list of characters that will be used to generate a password.
-    lower       = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                                    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+    lower       = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 
-    upper       = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                                    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+    upper       = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
 
-    number      = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+    number      = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 
-    punctuation = [ "`", "!", "\"", "$", "%", "^", "&", "*", "(", ")", "'",
-                                     "-", "_", "=", "+", "[", "{", "]", "}", ";", ":", "?",
-                                     "@", "#", "~", "\\", "|", ",", "<", ".", ">", "/" ]
+    punctuation = [ '`', '!', '"', '$', '%', '^', '&', '*', '(', ')', '\'',
+                    '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '?',
+                    '@', '#', '~', '\\', '|', ',', '<', '.', '>', '/' ]
 
     # Creating the characters variable that will, according to the security level
     # contain a list of characters from the above lists.
-    characters = dict()
+    characters = list()
 
     # Our quota at random.org.
     quota = None
@@ -62,7 +62,7 @@ class RandomPasswd:
     r = RandomDotOrg()
 
     def set_security(self, strength):
-        """Takes the given security strength and generates a mixed list in `characters`."""
+        """Takes the given security strength and generates a mixed list for `characters`."""
 
         if strength < 0 or strength > 4:
             raise ValueError("Please enter a strength between 0 and 4.")
@@ -82,7 +82,7 @@ class RandomPasswd:
         if strength == 4:
             self.characters += self.punctuation
 
-    def get_randnumbers(self, length):
+    def get_random_characters(self, length):
         """Retrives the random string of numbers for our password."""
 
         if type(length) is not int:
@@ -92,36 +92,21 @@ class RandomPasswd:
         self.quota = self.r.get_quota()
 
         if self.quota > 0:
-            data = list()
+            numbers = list()
 
-            data = self.r.randrange(0, len(self.characters), ammount = length)
+            numbers = self.r.randrange(0, len(self.characters), ammount = length)
         else:
             raise ValueError("Out of random.org bits...")
 
-        return data
+        characters = list()
 
-    def list_2_char(self, numbers):
-        """Converts the list of numbers into a list of characters using `characters`."""
-
-        data = list()
-
-        # shuffle the list of characters we are using.
+        # Shuffle the list of characters we are using.
         self.r.shuffle(self.characters)
 
         for i in numbers:
-            data.append(self.characters[i])
+            characters.append(self.characters[i])
 
-        return data
-
-    def gen_password(self, letters):
-        """Generates a string of the actual password from the list `letters`."""
-
-        password = str()
-
-        for i in letters:
-            password = password + i
-
-        return password
+        return characters
 
     def passwd(self, length = 14, strength = 4, amount = 1):
         """Generates a strong password with a default length of 14.
@@ -139,8 +124,10 @@ class RandomPasswd:
         if type(amount) is not int or amount < 1:
             raise ValueError("`amount` needs to be a positive integer.")
 
+        total_chars = length * amount
+
         # Avoid requesting too large an amount of random intgers.
-        if length * amount > 10000:
+        if total_chars > 10000:
             if length > amount:
                 raise ValueError("Please request a smaller password length.")
             else:
@@ -148,25 +135,16 @@ class RandomPasswd:
 
         self.set_security(strength)
 
-        rand     = self.get_randnumbers(length * amount)
-        letters  = self.list_2_char(rand)
+        random_characters = self.get_random_characters(total_chars)
 
-        # Splice up the list of letters into seperate passwords.
-        i       = 0
-        start   = 0
-        end     = 0
-
+        count = 0
         password = list()
 
-        while i < amount:
-            end = length * (i + 1)
-            password.append(self.gen_password(letters[start:end]))
-            start = start + length
-            i = i + 1
+        # Splice up the list of random_characters into seperate passwords.
+        while count < total_chars:
+            password.append(''.join(random_characters[count:count + length]))
 
-        # Return a string if there is only one password.
-        if i is 1:
-            password = str(password[0])
+            count += length
 
         return password
 
@@ -177,8 +155,8 @@ class RandomPasswd:
 if __name__ == "__main__":
     p = RandomPasswd()
 
-    length = 64
-    strength = 3
+    length = 128
+    strength = 4
     number = 1
 
     try:
@@ -197,13 +175,10 @@ if __name__ == "__main__":
             elif opt == "-n":
                     number = int(arg)
 
-    result =  p.passwd(length, strength, number)
+    result = p.passwd(length, strength, number)
 
-    if type(result) == str:
-        print(result)
-    else:
-        for password in result:
-             print(password)
+    for password in result:
+         print(password)
 
 def usage():
         print("Unknown arguments.")
