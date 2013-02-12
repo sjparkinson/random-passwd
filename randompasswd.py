f3ec6a620aec40876395046f4520232b4a79a11c
Sam Parkinson <sparkinson@outlook.com>
2013-02-12 22:49:34 +0000

    Fixed a couple more style errors.

--- randompasswd.py
+++ randompasswd.py
@@ -41,10 +41,10 @@ class RandomPasswd:
 
     # A list of characters that will be used to generate a password.
     lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -180,6 +180,7 @@ if __name__ == "__main__":
 
     for password in result:
         print(password)
+
 
 def usage():
         print("Unknown arguments.")
